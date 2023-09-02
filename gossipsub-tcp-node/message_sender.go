90dc8036041aec4c6072264cba417d5fb4cde58b
Jerry Tamlander <jerry@tamlander.fi>
2023-09-02 17:52:35 +0300

    initial code commit

---
+++ gossipsub-tcp-node/message_sender.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/peer"
+)
+
+func NewMessageSender(ctx context.Context, topic *pubsub.Topic, id peer.ID) error {
+
+	for {
+		message := generateRandomMessage(id)
+		// fmt.Println("Sending message:", message) // Add this line
+
+		msgBytes, err := json.Marshal(message)
+		if err != nil {
+			panic(err)
+		}
+
+		return topic.Publish(ctx, msgBytes)
+	}
+}
+
+func generateRandomMessage(id peer.ID) SingleMessage {
+	// Generate a random message (you can customize this logic)
+	message := SingleMessage{
+		Message: fmt.Sprintf("Random message at %s", time.Now().Format("2006-01-02 15:04:05")),
+		self:    id,
+		Sender:  id,
+	}
+	return message
+}
